cb9b50821c492ea9ff49939286da7df66e7eec65
YanPradoHub <yanprado146@gmail.com>
2026-06-09 10:31:10 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "logica.h"
+
+int gerar_numero_secreto() {
+    return 42;
+}
+
+int verificar_palpite(int numero_secreto, int palpite) {
+    if (palpite == numero_secreto) return 0;
+    if (palpite < numero_secreto)  return -1;
+    return 1;
+}
+
+void exibir_pistas(int numero_secreto) {
+    printf("PISTA: O numero e %s.\n", (numero_secreto % 2 == 0) ? "par" : "impar");
+    printf("PISTA: O numero esta entre %d e %d.\n", MINIMO, MAXIMO);
+}
